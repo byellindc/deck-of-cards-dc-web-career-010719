b06c5dee9995e1cf3d032e980b74a2c26fadd748
Ben Yellin <byellindc@gmail.com>
2019-01-24 09:28:17 -0500

    Done.

--- solution.rb
+++ solution.rb
@@ -1,7 +1,33 @@
-class Deck
+require 'pry'
 
+class Deck
+  attr_accessor :cards
+  
+  def initialize
+    self.cards = []
+
+    ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+    suits = ["Hearts", "Clubs", "Diamonds", "Spades"]
+
+    ranks.each do |rank|
+      suits.each do |suit|
+        cards << Card.new(suit, rank)
+      end
+    end
+  end
+
+  def choose_card
+    card = self.cards.sample
+    self.cards.delete(card)
+    # binding.pry
+    return card
+  end
 end
 
 class Card
+  attr_accessor :rank, :suit
 
+  def initialize(suit, rank)
+    self.suit, self.rank = suit, rank
+  end
 end
